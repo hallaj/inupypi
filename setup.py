1c97e89d3b380fbdd389ff9492d349198c074e12
Muhammad Hallaj bin Subery <hallajs@gmail.com>
2012-07-13 00:33:17 +0800

    updating the application version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='inupypi',
-        version='0.2.2',
+        version='0.2.3',
         packages=find_packages(),
         include_package_data=True,
         zip_safe=False,
