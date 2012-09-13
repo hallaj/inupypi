efc2f76df5f98335ca6e07e38e34700ba5263539
ogiaquino <ogiaquino@gmail.com>
2012-09-13 17:12:16 +0800

    change README adding how to setup on production environment

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
+import os
+os.environ["INUPYPI_SETTINGS"] = os.path.dirname(os.path.realpath(__file__)) + '/config.ini'
 from inupypi import app as application
-#application.run()
+application.run()
