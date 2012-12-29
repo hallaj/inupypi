dc3cb48d03cbcdc04cd23b38d1365586186ed79f
Low Kian Seong <kianseong@gmail.com>
2012-12-30 00:30:15 +0800

    Changed the inupypi_configure to use Path fully

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ multiple repositories.",
       test_suite='tests',
       install_requires=['Flask', 'Flask-Assets', 'argparse', 'cssmin',
                         'pkgtools', 'unipath'],
-      scripts=['inupypi_server','inupypi_configure.py'],
-      package_data={'conf_samples':['httpd-inupypi.conf.sample']})
+      scripts=['inupypi_server', 'inupypi_configure.py'],
+      package_data={'conf_samples': ['httpd-inupypi.conf.sample']})
