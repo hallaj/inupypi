db5e5a97a914bfbba8ab66c6bfa40be5e70b3875
lowks <kianseong@gmail.com>
2013-03-05 10:42:51 +0800

    Bumping up the version to 0.3 instead of 0.3-beta1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class PyTest(TestCommand):
 
 setup(
     name='inupypi',
-    version='0.3-beta1',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/hallaj/inupypi',
