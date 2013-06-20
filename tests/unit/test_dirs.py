6cbf2cf128a4e3cf42c5b644baa9760151eea11a
Low Kian Seong <kianseong@gmail.com>
2013-06-20 22:19:05 +0800

    re #33 fixing broken test

--- tests/unit/test_dirs.py
+++ tests/unit/test_dirs.py
@@ -11,6 +11,8 @@ class Test_Dirs(object):
         self.temp = tmpdir
         self.core_dirs = inupypi.core.Dirs(self.temp.strpath)
         self.dirs = ['dir1', 'dir2', 'dir3', 'dir4']
+        self.abs_dirs = [tmpdir.join(dir) for dir in
+                         self.dirs]
 
     def test_class_instance(self):
         assert(isinstance(self.core_dirs, inupypi.core.Dirs))
@@ -24,4 +26,5 @@ class Test_Dirs(object):
             self.core_dirs = inupypi.core.Dirs(self.temp.strpath)
 
             assert(self.core_dirs.__parents__ == self.temp.strpath)
+        for dir_ in self.abs_dirs:
             assert(dir_ in self.core_dirs.__contents__)
